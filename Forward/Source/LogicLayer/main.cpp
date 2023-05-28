e3a3ab111c0bd4be54b7d4b57ff78f7af7044a23
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-05-28 17:14:23 +0300

    Add translation

--- Forward/Source/LogicLayer/main.cpp
+++ Forward/Source/LogicLayer/main.cpp
@@ -1,22 +1,29 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QtWidgets/QApplication>
+#include <QtQml/QQmlApplicationEngine>
 
-#include <QIcon>
-#include <QQuickStyle>
+#include <QtGui/QIcon>
+
+#include <QtCore/QTranslator>
+#include <QtCore/QDebug>
 
 int main(int argc, char* argv[])
 {
-	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-	QGuiApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-	app.setApplicationName("Forward Desktop");
-	
-	// QQuickStyle::setFallbackStyle("Material");
-	// QQuickStyle::setStyle("Material");
+	QTranslator translator;
+    if (!translator.load(QLocale::system(), "lang", "_", ":/forward/langs"))
+		return -1;
+
+    QApplication::installTranslator(&translator);
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setApplicationVersion("0.0.1");
+	QApplication::setApplicationName("Forward Desktop");
+	QApplication::setWindowIcon(QIcon(":/forward/logo_64.png"));
 
 	QQmlApplicationEngine engine;
 
-    engine.load(":/forward/pages/App.qml");
-
+	engine.addImportPath(":/imports");
+    engine.load(":/imports/forward/App.qml");
+	
 	return app.exec();
 }
