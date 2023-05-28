35b1964966953af6a479ef1dbae2ab51c0527b04
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-05-28 17:12:26 +0300

    Add translation files

---
+++ Forward/Resources/langs/lang_en.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../../Source/FrontLayer/App.qml" line="83"/>
+        <source>New account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/App.qml" line="52"/>
+        <location filename="../../Source/FrontLayer/App.qml" line="90"/>
+        <source>Sing out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/App.qml" line="59"/>
+        <location filename="../../Source/FrontLayer/App.qml" line="97"/>
+        <source>Quit Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/App.qml" line="190"/>
+        <source>Program was minimized into tray.
+Click to open again</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <location filename="../../Source/FrontLayer/Login.qml" line="29"/>
+        <source>Choose Country...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/Login.qml" line="40"/>
+        <source>Phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/Login.qml" line="48"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/Login.qml" line="57"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Start</name>
+    <message>
+        <location filename="../../Source/FrontLayer/Start.qml" line="25"/>
+        <source>Select Language...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/Start.qml" line="38"/>
+        <source>Select Theme...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/Start.qml" line="51"/>
+        <source>Start messaging</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
