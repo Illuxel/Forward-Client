781b21dc0822f80db280833f3fe66a506743678e
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-19 00:42:21 +0300

    Update

---
+++ Forward/Resources/langs/lang_en_US.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="94"/>
+        <source>Sing out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="99"/>
+        <source>Quit Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="114"/>
+        <source>New account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="144"/>
+        <source>Program was minimized into tray. Click to open again</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="20"/>
+        <source>Please enter the credentials</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="31"/>
+        <source>Choose Country...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="42"/>
+        <source>Email adress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="55"/>
+        <source>Phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="71"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="81"/>
+        <source>Create account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="99"/>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="100"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Main.qml" line="121"/>
+        <source>Footer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Main.qml" line="132"/>
+        <source>Title %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Start</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="24"/>
+        <source>Select Language...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="48"/>
+        <source>Select Theme...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="74"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="75"/>
+        <source>Style will be changed after restarting application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="82"/>
+        <source>Start messaging</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ Forward/Resources/langs/lang_uk_UA.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="94"/>
+        <source>Sing out</source>
+        <translation type="unfinished">Вийти з запису</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="99"/>
+        <source>Quit Forward</source>
+        <translation type="unfinished">Закрити</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="114"/>
+        <source>New account</source>
+        <translation type="unfinished">Створити обілковий запис</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/App.qml" line="144"/>
+        <source>Program was minimized into tray. Click to open again</source>
+        <translation type="unfinished">Додаток був згорнутий в трей</translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="20"/>
+        <source>Please enter the credentials</source>
+        <translation>Введіть дані облікового запису</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="31"/>
+        <source>Choose Country...</source>
+        <translation>Виберіть країну...</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="42"/>
+        <source>Email adress</source>
+        <translation>Пошта</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="55"/>
+        <source>Phone number</source>
+        <translation>Номер телефону</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="71"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="81"/>
+        <source>Create account</source>
+        <translation>Створити обліковий запис</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="99"/>
+        <location filename="../../Source/FrontLayer/pages/Login.qml" line="100"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Main.qml" line="121"/>
+        <source>Footer</source>
+        <translation>Футер</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Main.qml" line="132"/>
+        <source>Title %1</source>
+        <translation>Тайтл %1</translation>
+    </message>
+</context>
+<context>
+    <name>Start</name>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="24"/>
+        <source>Select Language...</source>
+        <translation>Виберіть мову...</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="48"/>
+        <source>Select Theme...</source>
+        <translation>Виберіть тему...</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="74"/>
+        <source>Info</source>
+        <translation>Інформація</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="75"/>
+        <source>Style will be changed after restarting application</source>
+        <translation>Стиль додатку буде змінено після перезавантаження</translation>
+    </message>
+    <message>
+        <location filename="../../Source/FrontLayer/pages/Start.qml" line="82"/>
+        <source>Start messaging</source>
+        <translation>Почати спілкування</translation>
+    </message>
+</context>
+</TS>
