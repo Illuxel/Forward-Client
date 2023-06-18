9fb3598909f0e6b8cdd5614cc9a0fd5ef0ef1018
EEdle <83777873+EEdle506@users.noreply.github.com>
2023-06-19 00:41:50 +0300

    Update main.cpp

--- Forward/Source/LogicLayer/main.cpp
+++ Forward/Source/LogicLayer/main.cpp
@@ -1,29 +1,45 @@
 #include <QtWidgets/QApplication>
 #include <QtQml/QQmlApplicationEngine>
 
-#include <QtGui/QIcon>
-
-#include <QtCore/QTranslator>
 #include <QtCore/QDebug>
+
+#include <QtGui/QIcon>
+#include <QQuickStyle>
+#include <QtCore/QTranslator>
+#include <QtCore/QSettings>
 
 int main(int argc, char* argv[])
 {
-	QApplication app(argc, argv);
-
-	QTranslator translator;
-    if (!translator.load(QLocale::system(), "lang", "_", ":/forward/langs"))
-		return -1;
-
-    QApplication::installTranslator(&translator);
-	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QApplication::setApplicationVersion("0.0.1");
 	QApplication::setApplicationName("Forward Desktop");
+	QApplication::setOrganizationDomain("forchat.online");
+	QApplication::setOrganizationName("Forward Company Ltd.");
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+	QApplication app(argc, argv);
+	
 	QApplication::setWindowIcon(QIcon(":/forward/logo_64.png"));
+
+	{
+		QSettings settings (
+			QApplication::organizationName(),
+			QApplication::applicationName()
+		);
+
+		if (!settings.contains("style"))
+			settings.setValue("style", QQuickStyle::name());
+
+		auto style = settings.value("style").toString();
+		QQuickStyle::setStyle(style);
+	}
 
 	QQmlApplicationEngine engine;
 
 	engine.addImportPath(":/imports");
     engine.load(":/imports/forward/App.qml");
+
+	if (engine.rootObjects().isEmpty())
+        return -1;
 	
 	return app.exec();
 }
